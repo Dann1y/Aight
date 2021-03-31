357ca0140e2603da7c46ff99ada2ec23d8105c6a
dann1y <skypedanny@naver.com>
2021-03-31 20:19:19 +0900

    modify : image parsing

---
+++ src/utils/image-data.ts
@@ -0,0 +1,16 @@
+export const myImages = [
+  "my-image/1.JPG",
+  "my-image/2.JPG",
+  "my-image/3.JPG",
+  "my-image/4.JPG",
+  "my-image/5.JPG",
+  "my-image/6.JPG",
+  "my-image/7.JPG",
+];
+
+export const logoImages = [
+  "logo-image/Bridged.png",
+  "logo-image/Olio.png",
+  "logo-image/ONDO.png",
+  "logo-image/Charger.png",
+];
