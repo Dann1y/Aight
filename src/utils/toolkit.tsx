f5bde24c8f67b8875754fd44c3bfa71e6f8fa49b
dann1y <skypedanny@naver.com>
2021-04-22 15:04:05 +0900

    add : setting utils

---
+++ src/utils/toolkit.tsx
@@ -0,0 +1,27 @@
+import {
+  Eighth,
+  Eleventh,
+  Fifth,
+  First,
+  Fourth,
+  Ninth,
+  Second,
+  Seventh,
+  Sixth,
+  Tenth,
+  Third,
+} from "components";
+
+export const componentIndexing = {
+  0: <First />,
+  1: <Second />,
+  2: <Third />,
+  3: <Fourth />,
+  4: <Fifth />,
+  5: <Sixth />,
+  6: <Seventh />,
+  7: <Eighth />,
+  8: <Ninth />,
+  9: <Tenth />,
+  10: <Eleventh />,
+};
