3b8a0fd380aae4dfd2f531c56d164c9d43990ff1
dann1y <skypedanny@naver.com>
2021-04-24 17:29:49 +0900

    modify : add framer-motion

--- src/components/elements/second/index.tsx
+++ src/components/elements/second/index.tsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "@emotion/styled";
-import Fade from "react-reveal/Fade";
+import { motion } from "framer-motion";
+import aboutMotions from "motions/about.motion";
+import { useIntersection } from "utils/hooks/use-intersection";
 
 export default function Second() {
+  const sectionRef = useRef();
+
+  const { visible } = useIntersection(sectionRef, {
+    threshold: 1,
+    bottom: false,
+  });
+
   return (
-    <Positioner>
-      <Fade bottom>
-        <Sentence>Do it when you have time to think</Sentence>
-      </Fade>
+    <Positioner
+      ref={sectionRef}
+      initial="hidden"
+      animate={visible ? "visible" : "hidden"}
+    >
+      <Mask h={11}>
+        <Sentence variants={aboutMotions.subText}>
+          Do it when you have time to think
+        </Sentence>
+      </Mask>
+      <Mask h={3}>
+        <motion.h3 variants={aboutMotions.subText}>
+          yes, this is my motto
+        </motion.h3>
+      </Mask>
     </Positioner>
   );
 }
 
-const Positioner = styled.div`
+interface MaskProps {
+  h: number;
+}
+
+const Positioner = styled(motion.div)`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  position: relative;
+
+  h3 {
+    color: #fff;
+    font-weight: 100;
+  }
 `;
 
-const Sentence = styled.div`
+const Mask = styled.div<MaskProps>`
+  height: ${(p) => p.h}vh;
+  overflow: hidden;
+`;
+
+const Sentence = styled(motion.h2)`
   font-size: 8vh;
   color: #ffffff;
 `;
