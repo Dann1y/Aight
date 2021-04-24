2dba65f8d287749907052887d1987f89bdc94f8b
dann1y <skypedanny@naver.com>
2021-04-25 02:48:30 +0900

    modify : modify container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import React, { useEffect } from "react";
 import styled from "@emotion/styled";
-import BackgroundContainer from "containers";
-import { componentIndexing } from "utils/toolkit";
 import ReactFullpage from "@fullpage/react-fullpage";
+import MappingContainer from "containers";
 import { Footer } from "components";
-
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const SectionImages: Array<string> = importAll(
-  require.context("../../public/background", false, /.png$/)
-);
 
 function index() {
   return (
@@ -20,11 +11,7 @@ function index() {
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
-              {SectionImages.sort().map((i, ix) => (
-                <BackgroundContainer key={ix} imagePath={i}>
-                  <Wrapper>{componentIndexing[ix]}</Wrapper>
-                </BackgroundContainer>
-              ))}
+              <MappingContainer />
             </ReactFullpage.Wrapper>
           );
         }}
@@ -33,10 +20,5 @@ function index() {
     </div>
   );
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-`;
 
 export default index;
