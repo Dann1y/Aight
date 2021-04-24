36750f5261097a66ba46fa14e117af600ddd0814
dann1y <skypedanny@naver.com>
2021-04-24 15:25:49 +0900

    add : constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const DESC_LIST = [
+  {
+    title: "Trendy",
+    desc: `
+            I know what is trendy and I do.
+            I will prove it through the way I work.
+            Communicates well and results are good.You've never seen anyone as cool as
+            me😎
+`,
+  },
+  {
+    title: "Growing",
+    desc: `
+      Everyone makes mistakes.
+      And it's divided into two categories: fix mistakes or keep making
+      mistakes.
+      I’m the first one and grows to the next level.
+      Never get frustrated with mistakes.
+`,
+  },
+  {
+    title: "Fire",
+    desc: `
+      I'm on fire. This fire always makes great results.
+      See how much potential I have The fire is getting bigger`,
+  },
+];
