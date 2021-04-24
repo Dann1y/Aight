582db2e5e7e4bb81365985b9f659e2900f0b5653
dann1y <skypedanny@naver.com>
2021-04-25 02:48:12 +0900

    modify : mapping pages

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,27 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { componentsReturn } from "utils/toolkit";
 
-interface BackgroundContainerStyledProps {
-  p?: string;
-}
-
-interface BackgroundContainerProps {
-  imagePath: string;
-  children: React.ReactNode;
-}
-
-export default function BackgroundContainer(props: BackgroundContainerProps) {
-  const { imagePath, children } = props;
+export default function MappingContainer() {
   return (
-    <Container className="section" p={imagePath}>
-      {children}
-    </Container>
+    <Positioner>
+      {componentsReturn.map((pages, ix) => (
+        <div key={ix} className="section">
+          {pages}
+        </div>
+      ))}
+    </Positioner>
   );
 }
 
-const Container = styled.div<BackgroundContainerStyledProps>`
-  background-image: url(${(p) => p.p});
-  width: 100vw;
-  height: 100vh;
-  background-size: cover;
+const Positioner = styled.div`
+  width: 100%;
+  height: 100%;
 `;
