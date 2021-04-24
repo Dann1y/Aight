50a38c5b2cfd18f191bb68bf4ec934cb3aa8e40f
dann1y <skypedanny@naver.com>
2021-04-25 02:48:47 +0900

    modify : componentsReturn Array

--- src/utils/toolkit.tsx
+++ src/utils/toolkit.tsx
@@ -13,17 +13,17 @@ import {
   Third,
 } from "components";
 
-export const componentIndexing = {
-  0: <></>,
-  1: <First />,
-  2: <Second />,
-  3: <Third />,
-  4: <Fourth />,
-  5: <Fifth />,
-  6: <Sixth />,
-  7: <Seventh />,
-  8: <Eighth />,
-  9: <Ninth />,
-  10: <Tenth />,
-  11: <Eleventh />,
-};
+export const componentsReturn = [
+  <></>,
+  <First />,
+  <Second />,
+  <Third />,
+  <Fourth />,
+  <Fifth />,
+  <Sixth />,
+  <Seventh />,
+  <Eighth />,
+  <Ninth />,
+  <Tenth />,
+  <Eleventh />,
+];
