73548fd9e3e5a48146651798e84bd08b9c3d89f7
Dann1y <leedanny0102@gmail.com>
2022-10-30 03:40:04 +0900

    add : GetInTouchMe

--- src/components/getInTouchMe/GetInTouchMe.tsx
+++ src/components/getInTouchMe/GetInTouchMe.tsx
@@ -6,7 +6,7 @@ import { Card } from "../card/Card";
 
 export const GetInTouchMe = () => {
   return (
-    <Positioner width="100%" height="250px">
+    <Positioner width="90%" height="400px">
       <motion.h2 style={{}}>Get in touch me</motion.h2>
       <ContactWrapper>
         <ContactMethod bgColor="#e2fdff">
@@ -14,14 +14,29 @@ export const GetInTouchMe = () => {
             <Image src="/email.svg" width="50px" height="50px" alt="email" />
           </a>
         </ContactMethod>
-        <ContactMethod bgColor="#bfd7ff">
-          <Image src="/blog.svg" width="50px" height="50px" alt="email" />
+        <ContactMethod bgColor="#ffc8dd">
+          <a href="mailto: leedanny0102@gmail.com">
+            <Image src="/video.svg" width="50px" height="50px" alt="email" />
+          </a>
         </ContactMethod>
+
         <ContactMethod bgColor="#788bff">
-          <Image src="/linkedin.svg" width="50px" height="50px" alt="email" />
+          <a
+            href="https://www.linkedin.com/in/danny-lee17/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Image src="/linkedin.svg" width="50px" height="50px" alt="email" />
+          </a>
         </ContactMethod>
         <ContactMethod bgColor="#5465ff">
-          <Image src="/github.svg" width="50px" height="50px" alt="email" />
+          <a
+            href="https://github.com/dann1y"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Image src="/github.svg" width="50px" height="50px" alt="email" />
+          </a>
         </ContactMethod>
       </ContactWrapper>
     </Positioner>
@@ -33,6 +48,10 @@ const Positioner = styled(Card)`
   align-items: flex-start;
   justify-content: space-between;
   padding: 20px 30px 30px 30px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  max-width: 1200px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
   ${MEDIA_QUERY.TABLET} {
     height: auto;
@@ -42,11 +61,13 @@ const Positioner = styled(Card)`
 
 const ContactWrapper = styled.div`
   display: flex;
+  flex-grow: 1;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  cursor: pointer;
+  padding-top: 5%;
   gap: 16px;
+  cursor: pointer;
 
   ${MEDIA_QUERY.TABLET} {
     margin-top: 20px;
@@ -60,10 +81,18 @@ const ContactWrapper = styled.div`
 const ContactMethod = styled(Card)<{ bgColor?: string }>`
   position: relative;
   width: 100%;
-  height: 140px;
+  height: 100%;
   background-color: ${({ bgColor }) => bgColor};
   border-radius: 20px;
   transform-origin: 50% 50% 0;
+
+  a {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
   &:hover {
     width: 90%;
